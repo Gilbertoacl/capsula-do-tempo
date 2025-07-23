c54660fbc3916c7a066978bca7f52777acd2aab6
Gilbertoacl <betoacl@hotmail.com>
2025-07-23 11:45:40 -0300

    feat: Adicionado o Projeto inicial

---
+++ capsula-do-tempo/src/main/java/com/gilberto/capsula_do_tempo/CapsulaDoTempoApplication.java
@@ -0,0 +1,13 @@
+package com.gilberto.capsula_do_tempo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CapsulaDoTempoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CapsulaDoTempoApplication.class, args);
+	}
+
+}
---
+++ capsula-do-tempo/src/test/java/com/gilberto/capsula_do_tempo/CapsulaDoTempoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gilberto.capsula_do_tempo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CapsulaDoTempoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
