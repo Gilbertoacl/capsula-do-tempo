1a20687b1b3074e5d24666e95b19a9e163812735
Gilbertoacl <betoacl@hotmail.com>
2025-07-23 16:23:08 -0300

    feat: Adicionado o Repository de Capsula

---
+++ capsula-do-tempo/src/main/java/com/gilberto/capsula_do_tempo/repository/CapsulaRepository.java
@@ -0,0 +1,9 @@
+package com.gilberto.capsula_do_tempo.repository;
+
+import com.gilberto.capsula_do_tempo.models.Capsula;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CapsulaRepository extends JpaRepository<Capsula, Long> {
+
+
+}
